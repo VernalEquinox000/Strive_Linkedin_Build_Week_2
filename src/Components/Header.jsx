f35f0824f0af73c2a99cbea2ae2e74d38aa7643a
VernalEquinox <pieroapruzzese@gmail.com>
2020-11-30 17:15:01 +0100

    header almost done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import {Container, Row, Col, Card} from 'react-bootstrap'
+import { Container, Row, Col, Card } from 'react-bootstrap'
+import Background from "../Assets/linkedin_bg.jpeg"
 
 export default class Header extends Component {
 
@@ -44,11 +45,21 @@ export default class Header extends Component {
             <div>
                 <Container>
                     <Row>
-                        <Col>
+                        <Col xs={12}>
                             <Card>
-                                <Card.Img variant="top" src={this.state.profile && this.state.profile.image} />
+                                <Card.Img variant="top" src={Background} style={{ objectFit: "cover", height: "180px" }}/>
                                 <Card.Body>
-                                <Card.Text>
+                                    <Card.Text style={{fontSize: "30px", fontWeight:"bolder"}}>{this.state.profile && this.state.profile.name}{' '}
+                                        {this.state.profile && this.state.profile.surname}
+                                    </Card.Text>
+                                    <Card.Text>
+                                        {this.state.profile && this.state.profile.title}
+                                    </Card.Text>
+                                    <Card.Text style={{color: "grey"}}>
+                                        {this.state.profile && this.state.profile.area}{'        '} <a href="#" style={{fontWeight:"bolder"}}>129 links</a>
+                                </Card.Text>
+                                <Card.Text style={{color: "grey"}}>
+                                        {this.state.profile && this.state.profile.area}{'        '} <a href="#" style={{fontWeight:"bolder"}}>129 links</a>
                                 </Card.Text>
                                 </Card.Body>
                             </Card>
