67b8a931c404fba6653c59bc4718b987802304e6
Manuel Desole <46155957+madnol@users.noreply.github.com>
2020-11-30 12:37:34 +0100

    nav fix

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBarr from "./Components/NavBarr";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarr />
+    </div>
+  );
+}
+
+export default App;
