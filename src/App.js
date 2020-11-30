4724ba8620b8ba9639e2f9d9f62a1d7d94b42704
enisazizi <enisazizi@hotmail.com>
2020-11-30 18:35:09 +0100

    yup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 
-import NavBar from "./Components/NavBar";
-import Footer from "./Components/Footer";
-import "./navbar.css";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Aside from "./Components/Aside"
 
 
 function App() {
   return (
     <div className="App">
 
-      <NavBar />
-
-      <Footer />
+      <Aside />
 
     </div>
   );
