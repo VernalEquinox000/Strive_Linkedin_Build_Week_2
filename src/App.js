cff8b345630e6d702ee92c444fbbb620f37b88de
HolaSoyGonzalo <daved.pacco@gmail.com>
2020-12-03 22:48:17 +0100

    whatever

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Header />
-      {/* <NewsFeed /> */}
+      {/* <Header /> */}
+      <NewsFeed />
       <Footer />
     </div>
   );
